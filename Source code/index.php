781a54572172111e8439e618e67c9b5703fe6446
Rafal Ozog <rafalozog@gmail.com>
2020-03-21 22:53:58 +0000

    Main page re-freshement.

--- Source code/index.php
+++ Source code/index.php
@@ -3,27 +3,36 @@
 <head>
 
 	<title>Elegant and cheap websites for business</title>
-	
+
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="description" content="Creation of elegant and useful websites, which increase business value, attract customers, make an enterprise open and attainable." />
 	<meta name="keywords" content="elegant-website, elegant-websites, websites, website, elegant, web-development, creation, rafal, ozog, business, customers, enterprise, attainable" />
-	
+
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="css/style.css" rel="stylesheet" type="text/css" />
-	
+
 	<link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Quicksand:400,500" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-	
+
 </head>
 
 <body>
 
 	<div id="logo" class="container">
-		<a href="/" class="links_no_decoration">Elegant Website for Your Business</a>
+		<a href="index.php" class="links_no_decoration">Elegant Website for Your Business</a>
+
+		<div id="nav_bar">
+			<ul>
+				<li><a href="index.php" style="color: grey;">Home</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="order.php">Order</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</div>
 	</div>
-	
+
 	<div id="benefits">
 		<div class="container">
 		<div class="row">
@@ -31,7 +40,7 @@
 					<img class="img-thumbnail" src="img/c2.jpg">
 				</div>
 				<div class="col-md-8">
-					Do you have a small or medium-size business? My name is Rafal and I would like to offer you a neat, intuitive and good-looking website for your business! 
+					Do you have a small or medium-size business? My name is Rafal and I would like to offer you a neat, intuitive and good-looking website for your business!
 					<br/><br/>
 					You don't have to be worried about design and technical issues. I will prepare a project, implement fully-working website and publish it for you.
 					<br/><br/>
@@ -46,14 +55,14 @@
 			</div>
 		</div>
 	</div>
-	
+
 	<div id="introduction">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8">
 					<span style="font-size:1.2rem">How does it work?</span>
 					<br/><br/>
-					1. Specify what design and functional features you expect. 
+					1. Specify what design and functional features you expect.
 					<br/>
 					Don't be worried if you don't have any idea - I'll prepare the layout project for you <br/><strong> free of charge</strong>.
 					<br/>
@@ -64,7 +73,7 @@
 					3. If you like the final effect, the website will be published!
 					<br/><br/>
 					<div class="text-center">
-						<a class="btn btn-success" style="width: 280px; height: 35px; padding-top: 7px; border-radius: 8px;" href="order_elegant_website.php">Start the Process Now</a>
+						<a class="btn btn-success" style="width: 280px; height: 35px; padding-top: 7px; border-radius: 8px;" href="order.php">Start the Process Now</a>
 					</div>
 				</div>
 				<div class="col-md-4 text-center">
@@ -73,8 +82,8 @@
 			</div>
 		</div>
 	</div>
-	
-	<div id="examples">
+
+	 <script>/* <div id="examples">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
@@ -94,13 +103,13 @@
 				</div>
 			</div>
 		</div>
-	</div>
-	
-	<div id="footer">
+	</div> */</script>
+
+	<div id="footer" class="footer-reversed-colors">
 		<div class="container">
-			All rights reserved 2017 &copy Elegant Website for Your Business - Rafal Ozog Web Development, Edinburgh
+			All rights reserved 2020 &copy Elegant Websites for Your Business - Rafal Ozog Web Development, London
 		</div>
 	</div>
-	
+
 </body>
 </html>
